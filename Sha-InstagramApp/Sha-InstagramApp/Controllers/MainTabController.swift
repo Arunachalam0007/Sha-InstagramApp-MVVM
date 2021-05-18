6b06cf98f741416caac0a2c1014f4f3048e60b96
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-18 13:02:45 +0530

    Created and Set MainTabController is RootViewController

---
+++ Sha-InstagramApp/Sha-InstagramApp/Controllers/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  Sha-InstagramApp
+//
+//  Created by ArunSha on 18/05/21.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+    
+}
