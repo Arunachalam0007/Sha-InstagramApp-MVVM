b6b08566996778109ec6bb2456a25b0f09c2d737
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-20 12:12:17 +0530

    AuthenticationVM added protocol for Auth and FormVM for some properties used in both login and registraionVM

--- Sha-InstagramApp/Sha-InstagramApp/View Models/AuthenticationViewModel.swift
+++ Sha-InstagramApp/Sha-InstagramApp/View Models/AuthenticationViewModel.swift
@@ -7,17 +7,37 @@
 
 import UIKit
 
-struct LoginViewModel {
+protocol FormViewModel {
+    func updateFormBtn()
+}
+
+protocol AutheticationViewModel {
+    var btnIsValid:Bool { get }
+    var btnBackgroundColor:UIColor { get }
+}
+
+struct LoginViewModel: AutheticationViewModel {
     var email:String?
     var password:String?
-    var isValid:Bool {
+    var btnIsValid:Bool {
         return email?.isEmpty == false && password?.isEmpty == false
     }
     var btnBackgroundColor:UIColor {
-        return isValid ? .systemPurple.withAlphaComponent(1) : .systemPurple.withAlphaComponent(0.2)
+        return btnIsValid ? .systemPurple.withAlphaComponent(1) : .systemPurple.withAlphaComponent(0.2)
     }
 }
 
-struct RegistrationViewModel {
+struct RegistrationViewModel: AutheticationViewModel {
+    var email: String?
+    var password: String?
+    var fullname: String?
+    var username: String?
     
+    var btnIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false && fullname?.isEmpty == false && username?.isEmpty == false
+    }
+    
+    var btnBackgroundColor: UIColor {
+        return btnIsValid ? .systemPurple.withAlphaComponent(1) : .systemPurple.withAlphaComponent(0.2)
+    }
 }
